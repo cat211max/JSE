c269067c2121af75b454437586dd4082a483c984
HowardYao <yidanyao@gmail.com>
2025-12-17 01:58:35 +0800

    firt commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+// index.js
+// This is the main entry point of our application
+
+const depthLimit = require('graphql-depth-limit');
+const {createComplexityLimitRule} = require('graphql-validation-complexity');
+
+const helmet = require('helmet'); 
+const cors = require('cors'); 
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express'); 
+const jwt = require('jsonwebtoken'); 
+
+const getUser = token =>{
+    if(token){
+        try{
+            return jwt.verify(token, process.env.JWT_SECRET);
+        }catch(err){
+            throw new Error('Session invalid');
+        }
+    }
+};
+
+require('dotenv').config();
+
+
+const db = require('./db');
+const Note = require('./models/note');
+const { User } = require('./models');
+const models = {Note,User} ;
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers'); 
+const { get } = require('mongoose');
+
+
+
+
+
+const port = process.env.PORT || 4000; 
+const DB_HOST = process.env.DB_HOST; 
+
+const app = express();
+
+app.use(helmet()); 
+app.use(cors()); 
+db.connect(DB_HOST);
+
+
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+    validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
+    context: ({req})=>{
+        const token = req.headers.authorization;
+        const user = getUser(token);
+        console.log(user);
+        return {models,user};
+    }
+});
+
+server.applyMiddleware({app,path:'/api'});
+
+
+app.listen({port}, () => 
+    console.log(
+        `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+    )
+);
+
+
+
